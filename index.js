96a8ff1021f13e3afd8da8812ca3346f963040e2
iannjau <lantanacreations@gmail.com>
2024-03-15 20:48:03 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+function add(a,b) {
+  return "81"
+}
+function subtract() {
+   return "20"
+}
+function multiply() {
+    return "6.8"
+}
+function divide() {
+    return "2"
+
+}
+function add( a,b){
+    return a + b;
+}
+function subtract(b,a){
+    return b - a;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide (a,b){
+    return a / b;
+}
+parseInt("2", 10); //=> 2
